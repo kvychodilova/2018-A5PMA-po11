61003a15b411b683b3d85a6b4310ab4400250ad7
Student <student@iMac-B202-03.local>
2018-10-08 12:06:37 +0200

    HomePage First UI _ USER input

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -7,8 +7,17 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
+  private userInput: string;
+  private result: string;
+
+
   constructor(public navCtrl: NavController) {
 
   }
 
+  private btnTranslateClicked(input:string):void{
+    console.log(input);
+    this.userInput = input;
+
+  }
 }
