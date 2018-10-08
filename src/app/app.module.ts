145fe2d70b1797e077d5c0f2ddabf9837c30ab5e
Student <student@iMac-B202-03.local>
2018-10-08 12:36:52 +0200

    Provider for HTTP client module added to APPmodules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule, ErrorHandler } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {HttpClientModule} from '@angular/common/http';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
 
@@ -22,6 +23,7 @@ import { TranslationProvider } from '../providers/translation/translation';
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     IonicModule.forRoot(MyApp)
   ],
   bootstrap: [IonicApp],
